5406fbe1c918b04d29c721319084254d06489d76
Shah Jalal <rjshahjalal132@gmail.com>
2025-03-07 17:05:16 +0600

    add codestar framework

--- loader.php
+++ loader.php
@@ -2,3 +2,6 @@
 
 // include helper functions
 include_once PLUGIN_BASE_PATH . '/inc/helpers/helper-functions.php';
+
+// require codestar framework
+require_once PLUGIN_BASE_PATH . '/inc/libs/codestar-framework/codestar-framework.php';
