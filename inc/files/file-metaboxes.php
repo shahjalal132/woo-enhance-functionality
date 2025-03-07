69a2c45d3b6c93a1ccfa14a23da8ea460f87c8f7
Shah Jalal <rjshahjalal132@gmail.com>
2025-03-07 19:05:36 +0600

    create dropdowns metabox

---
+++ inc/files/file-metaboxes.php
@@ -0,0 +1,56 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) {
+    die;
+} // Cannot access directly.
+
+if ( class_exists( 'CSF' ) ) {
+
+    // Prefix
+    $prefix = "_dropdowns";
+
+    // Create metabox
+    CSF::createMetabox( $prefix, array(
+        'title'        => 'Dropdown Configurations',
+        'post_type'    => 'product',
+        'show_restore' => true,
+    ) );
+
+    CSF::createSection( $prefix, array(
+        'title'  => 'Manage Dropdowns',
+        'icon'   => '',
+        'fields' => array(
+
+            // Repeater field
+            array(
+                'id'     => 'outer_dropdown_repeater',
+                'type'   => 'repeater',
+                'title'  => 'Dropdowns',
+                'fields' => array(
+                    // dropdown name key field
+                    array(
+                        'id'          => 'outer_dropdown_name',
+                        'type'        => 'text',
+                        'title'       => 'Dropdown Name',
+                        'placeholder' => 'Dropdown Name',
+                    ),
+                    // dropdown options
+                    array(
+                        'id' => 'inner_dropdown_items',
+                        'type' => 'repeater',
+                        'title' => 'Dropdown Items',
+                        'fields' => array(
+                            // dropdown name key field
+                            array(
+                                'id'          => 'inner_dropdown_name',
+                                'type'        => 'text',
+                                'title'       => 'Item Name',
+                                'placeholder' => 'Item Name',
+                            ),
+                        ),
+                    )
+                ),
+            ),
+        ),
+    ) );
+}
--- loader.php
+++ loader.php
@@ -4,4 +4,7 @@
 include_once PLUGIN_BASE_PATH . '/inc/helpers/helper-functions.php';
 
 // require codestar framework
-require_once PLUGIN_BASE_PATH . '/inc/libs/codestar-framework/codestar-framework.php';
+include_once PLUGIN_BASE_PATH . '/inc/libs/codestar-framework/codestar-framework.php';
+
+// include metabox
+include_once PLUGIN_BASE_PATH . '/inc/files/file-metaboxes.php';
